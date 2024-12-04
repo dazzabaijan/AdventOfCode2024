b75d27b7c74451b1ff09030540c783d4993de99c
dazzabaijan <darrenng97@Darren>
2024-12-04 23:49:32 +0000

    added day2 part2 solution

--- day2/solutionP2.cpp
+++ day2/solutionP2.cpp
@@ -36,14 +36,13 @@ void split(std::string& line, std::vector<int>& result)
 		i++;
 	}
 	result.push_back(std::stoi(value));
-	// for (int j=0; j<result.size(); j++) std::cout << result[j] << " ";
-	// std::cout << "\n";
+	//for (int j=0; j<result.size(); j++) std::cout << result[j] << " "; std::cout << "\n";
 	return;
 }
 
 std::vector<std::vector<int>> readInput(std::string fileName)
 {
-	std::string fileLoc = "/home/darrenng97/develop/cpp/AdventOfCode/2024/day2/" + fileName + ".txt";
+	std::string fileLoc = "/home/darrenng97/develop/cpp/AdventOfCode2024/day2/" + fileName + ".txt";
 	std::fstream inFile(fileLoc);
 	
 	std::string line;
@@ -60,28 +59,69 @@ std::vector<std::vector<int>> readInput(std::string fileName)
 	return reports;
 }
 
+bool reportIsSafe(std::vector<int> report)
+{
+	int diff;
+	bool onlyInc = true, onlyDec = true, diffWithinBounds = true;
+	for (int i=0; i<report.size()-1; i++)
+	{
+		diff = report[i] - report[i+1];
+
+		if (abs(diff) < 1 || abs(diff) > 3)
+		{
+			diffWithinBounds = false;
+			break;
+		}
+		if (diff > 0)
+			onlyInc = false;
+		if (diff < 0)
+			onlyDec = false;
+	}
+	bool isSafe = diffWithinBounds && (onlyInc || onlyDec);
+	return isSafe;
+}
+
 int solution(char** argv)
 {
 	std::vector<std::vector<int>> reports = readInput(argv[1]); 
 	int safeReports = 0;
-
+	
 	for (const std::vector<int>& report : reports)
 	{
-		bool safe = true, dampenerUsed = false;
-		int diff = 0, nextDiff = 0;
-		for (int j=0; j<report.size()-2; j++)
+		bool safe = false;
+		int diff;
+		
+		auto considerRemoveAtIndex = [&](int j)
+		{
+			std::vector<int> tmp = report;
+			tmp.erase(tmp.begin() + j);
+			if (reportIsSafe(tmp))
+				safe = true;
+		};
+
+		considerRemoveAtIndex(0);
+		for (int j=0; j<report.size()-1; j++)
 		{
 			diff = report[j] - report[j+1];
-			nextDiff = report[j+1] - report[j+2];
-
-			if (abs(diff) < 1 || abs(diff) > 3 ||
-				abs(nextDiff) < 1 || abs(nextDiff) > 3 ||
-				(diff > 0 && nextDiff < 0) || (diff < 0 && nextDiff > 0))
+			if (abs(diff) < 1 || abs(diff) > 3)
 			{
-				safe = false;
+				// can we use dampener to remove j or j+1?
+				considerRemoveAtIndex(j);
+				considerRemoveAtIndex(j+1);
 				break;
 			}
-			// if (diff > 0 &&)
+			
+			if (j+2 < report.size()){
+				int nextDiff = report[j+1] - report[j+2];
+				if ((diff > 0 && nextDiff <0) || (diff < 0 && nextDiff > 0))
+				{
+					// for peak/trough can we use dampener to remove j, j+1 or j+2?
+					considerRemoveAtIndex(j);
+					considerRemoveAtIndex(j+1);
+					considerRemoveAtIndex(j+2);
+					break;	
+				}
+			}
 		}
 		if (safe)
 			safeReports++;
